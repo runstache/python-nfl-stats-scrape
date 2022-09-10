7bac958838b87110b1721190bc1b9a9e4749f938
Larry Shields <larry.shields@gmail.com>
2022-09-10 08:34:24 -0400

    added parameter for the year

--- src/pull_stats_data.sh
+++ src/pull_stats_data.sh
@@ -1,14 +1,12 @@
 input_week=1
-input_year=2017
+input_year=$1
 input_type=2
 
-while [ $input_year -lt 2022 ]; do
 
-  while [ $input_week -lt 18 ]; do
-    echo "Pulling games for $input_year week $input_week"
-    command="python stats_loader.py -w $input_week -y $input_year -t $input_type"
-    eval $command
-    ((input_week+=1))
-  done
-  ((input_year+=1))
+
+while [ $input_week -lt 18 ]; do
+  echo "Pulling games for $input_year week $input_week"
+  command="python stats_loader.py -w $input_week -y $input_year -t $input_type"
+  eval $command
+  ((input_week+=1))
 done
