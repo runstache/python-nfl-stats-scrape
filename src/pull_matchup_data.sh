c9d949d71cad8c09ab35a135ab37440de2133dc7
Larry Shields <larry.shields@gmail.com>
2022-09-11 13:29:52 -0400

    Updated scripts with command line arguments

--- src/pull_matchup_data.sh
+++ src/pull_matchup_data.sh
@@ -1,13 +1,11 @@
 input_week=$2
 input_year=$1
-input_type=$4
 max_week=$3
-
-
+input_type=$4
 
 while [ $input_week -lt $max_week ]; do
   echo "Pulling games for $input_year week $input_week"
-  command="python stats_loader.py -w $input_week -y $input_year -t $input_type"
+  command="python matchup_loader.py -w $input_week -y $input_year -t $input_type"
   eval $command
   ((input_week+=1))
 done
